38a469ff80deb62396578ee16b57345038fa1b33
AntonioErick <147534096+AntonioErick@users.noreply.github.com>
2023-11-29 14:36:44 -0300

    Add files via upload

---
+++ questao3.c
@@ -0,0 +1,14 @@
+# include < stdio .h >
+
+int main ( ) {
+//Não foi declarado a variável i
+int N_ant , N_prox , N , N_atual//Está faltando o ";"
+N_ant = N_atual =1;
+for( i =1 ,i <= N , i ++) {//erro no laço de repetição
+N_prox = N_ant + N_atual ;
+N_ant = N_atual ;
+N_atual = N_prox ;
+printf ("Fim do laco !\n") ;
+return//está faltando o 0
+}
+
